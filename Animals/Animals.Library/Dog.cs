b5ca29fd7275f7a192bab9879d60cd6635b5b6f5
Matt Kagel <mpkagel@gmail.com>
2019-02-19 14:03:43 -0600

    Commit more animals

--- Animals/Animals.Library/Dog.cs
+++ Animals/Animals.Library/Dog.cs
@@ -2,18 +2,51 @@ using System;
 
 namespace Animals.Library
 {
-    public class Dog 
+    public class Dog
     {
 
-    public string Noise = "Woof!";
+        internal string Noise = "Woof!";
+        public string getNoise()
+        {
+            return Noise;
+        }
 
-        public void GoTo(string location) 
+        public void setNoise(string newValue)
+        {
+            if (newValue == null || newValue.Length == 0)
+            {
+                throw new ArgumentException("value must not be null or empty");
+            }
+
+            Noise = newValue;
+        }
+
+        
+
+        private string _name;
+
+        public string Name 
+        { 
+            get 
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            } 
+        }
+
+        public string Color { get; } = "brown";
+        public string Breed { get; set; }
+
+        public void GoTo(string location)
         {
             //Console.WriteLine("Walking to " + location);
 
             Console.WriteLine($"Walking to {location}");
 
-            
+
         }
 
         public void MakeNoise()
